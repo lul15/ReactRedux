6083f468396daa7ff5a47d95b0a28ee235a3f4f9
Lu <lliu@trinity.edu>
2016-07-12 22:31:19 -0500

    Created new 'src' folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new component.
+// This component should produce some HTML
 
-import App from './components/app';
-import reducers from './reducers';
+/*
+const - ES2016
+  declaring a variable, this is the final constant, never going to be re-assigned
+App - assigned to a function
+<div>Hi!</div> - JSX
+  JSX - uses webpack and Babel to transpile into JavaScript
+*/
+const App = function() {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Take the component's generated HTML and put it on the page (in the DOM)
+//DOM - Document Object Model, structure determines how HTML is rendered
