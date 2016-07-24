2b546e62499f3738e54205e3d0731439f47a8f40
Lu <lliu@trinity.edu>
2016-07-24 17:52:36 -0500

    Lesson 16: added search_bar.js - changed from functional component to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react'; //{} pulls the Component from React library
 
+/*
+JSX function
+SearchBar - functional component
+*/
+/*
 const SearchBar = () => {
   return <input />;
 };
+*/
+
+/*
+Class-based component, always need a render and a return
+define new class SearchBar, give it all the functionality that React.component has
+*/
+class SearchBar extends Component{
+  render() {
+    return <input />;
+  }
+}
 
 /*
 exports the SearchBar function
