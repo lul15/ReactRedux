20a691c6c2f9270efd9ceac15ad0a4afbb2ac0ff
Lu <lliu@trinity.edu>
2016-09-12 07:38:21 -0500

    Lesson 20: breather and review

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,11 +12,27 @@ const SearchBar = () => {
 
 /*
 Class-based component, always need a render and a return
+ONLY class-based components have state
 define new class SearchBar, give it all the functionality that React.component has
 */
 class SearchBar extends Component{
+  //constructor called automatically when the instance of the class is created
+  constructor(props) {
+    //calling parent class
+    super(props);
+
+    this.state = {term: ''};
+  }
+
   render() {
-    return <input />;
+    //setState = manipulate state
+    return (
+      <div>
+      <input
+        value = {this.state.term}
+        onChange={(event) => this.setState({term: event.target.value})}/>
+      </div>
+    );
   }
 }
 
