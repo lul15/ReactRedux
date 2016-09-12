048b6319c4a3bd38c2eefacc66f03c2fd60e0a45
Lu <lliu@trinity.edu>
2016-09-12 10:37:55 -0500

    Returns video list length

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React {Component} from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = "AIzaSyBwygzPMQ3ZwppySQuo_Vs7cj7zLbjIpzA";
 
 
@@ -15,7 +15,7 @@ App - assigned to a function, it's a class, not an instance of the component
   need to be instantiated before being passed
 */
 //changed from functional to class -component, in order to use state
-class App extends Component = () => {
+class App extends Component {
   constructor(props) {
     super(props);
 
@@ -24,7 +24,6 @@ class App extends Component = () => {
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({videos});
       //ES6: this.setStatate({videos}) = this.setState({videos: videos}); prop and value has to have same name
-      
     });
   }
 
@@ -32,6 +31,7 @@ class App extends Component = () => {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
         </div>
       );
     }
@@ -40,10 +40,4 @@ class App extends Component = () => {
 
 //Take the component's generated HTML and put it on the page (in the DOM)
 //DOM - Document Object Model, structure determines how HTML is rendered
-/*
-<App></App> JSX tags make a component instance
-<App /> self-closing tag
-render(HTML, HTML_node where to put HTML)
-document.querySelector - finds the id of the container, and tries to render the HTML there
-*/
 ReactDOM.render(<App />, document.querySelector('.container'));
