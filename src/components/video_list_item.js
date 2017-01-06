f9d8de3b9188853f72d2f20579ec5e4cf57b9a70
Lu <lliu@trinity.edu>
2017-01-06 12:31:47 -0600

    Lecture 28 - added video list component

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   //const video = props.video; //same as above without props
-
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className="list-group-item">
+    <li onClick{() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl}/>
--- src/index.js
+++ src/index.js
@@ -20,10 +20,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
       //ES6: this.setStatate({videos}) = this.setState({videos: videos}); prop and value has to have same name
     });
   }
@@ -32,8 +38,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
         </div>
       );
     }
