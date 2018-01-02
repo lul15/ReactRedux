5dec672606a9cfc8f03d9349d4d3909d829e8c8a
Lu <lliu@trinity.edu>
2018-01-02 15:44:31 -0600

    lesson 39 - reducers

--- src/index.js
+++ src/index.js
@@ -1,62 +1,15 @@
-import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
-const API_KEY = "AIzaSyBwygzPMQ3ZwppySQuo_Vs7cj7zLbjIpzA";
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
+import App from './components/app';
+import reducers from './reducers';
 
-/*
-const - ES2016
-  declaring a variable, this is the final constant, never going to be re-assigned
-App - assigned to a function, it's a class, not an instance of the component
-  need to be instantiated before being passed
-Class - uses state
-*/
-class App extends Component {
-  constructor(props) {
-    super(props);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    };
-
-    this.videoSearch('surfboards');
-  }
-
-  videoSearch(term) {
-    YTSearch({key: API_KEY, term: term}, (videos) => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-      /*ES6: this.setState({videos}) = this.setState({videos: videos});
-      prop and value has to have same name*/
-    });
-  }
-
-  render() {
-    /*debounce = takes a new function,
-    and returns a new function that is called every 300 milliseconds
-    */
-    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
-
-    return (
-      <div>
-        <SearchBar onSearchTermChange={videoSearch}/>
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-          videos={this.state.videos} />
-        </div>
-      );
-    }
-}
-
-
-//Take the component's generated HTML and put it on the page (in the DOM)
-//DOM - Document Object Model, structure determines how HTML is rendered
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store = {createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+, document.querySelector('.container'));
